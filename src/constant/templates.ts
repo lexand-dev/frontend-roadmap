258ce208d0ee64a9b11f04d933953b8cb55273ab
alexander flores <alexfloresrayme@gmail.com>
2024-09-29 00:35:47 -0500

    feat:add all sections

---
+++ src/constant/templates.ts
@@ -0,0 +1,29 @@
+import Check from "../components/icons/check.astro";
+
+export const templates = [
+  {
+    name: "Habit tracker",
+    icon: Check,
+    url: "",
+  },
+  {
+    name: "Habit tracker",
+    icon: Check,
+    url: "",
+  },
+  {
+    name: "Habit tracker",
+    icon: Check,
+    url: "",
+  },
+  {
+    name: "Habit tracker",
+    icon: Check,
+    url: "",
+  },
+  {
+    name: "Habit tracker",
+    icon: Check,
+    url: "",
+  },
+];
